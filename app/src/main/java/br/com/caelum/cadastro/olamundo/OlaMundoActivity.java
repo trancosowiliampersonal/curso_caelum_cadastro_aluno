c044ca2d01b2c83cfb4944aa327295208d0d829d
wiliamtrancoso <wileleh@gmail.com>
2016-03-02 15:30:11 -0300

    [master] [wiliam.trancoso]   \_> 1.6 Ciclo de vida

--- app/src/main/java/br/com/caelum/cadastro/olamundo/OlaMundoActivity.java
+++ app/src/main/java/br/com/caelum/cadastro/olamundo/OlaMundoActivity.java
@@ -2,6 +2,7 @@ package br.com.caelum.cadastro.olamundo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 import br.com.caelum.cadastro.R;
 
@@ -14,5 +15,43 @@ public class OlaMundoActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ola_mundo);
+
+        Log.i("CICLO DE VIDA", "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("CICLO DE VIDA", "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("CICLO DE VIDA", "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("CICLO DE VIDA", "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("CICLO DE VIDA", "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("CICLO DE VIDA", "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("CICLO DE VIDA", "onRestart");
     }
 }
