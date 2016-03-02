f1c51fc1d6ed2c21dcde56b983e2afe0082c19c4
wiliamtrancoso <wileleh@gmail.com>
2016-03-02 18:19:05 -0300

    [aula] [wiliam.trancoso]   \_> 2.1 Criando Projeto [Incompleto]     \_> criacao da lista aluno       \_> activity java       \_> activity layout xml

---
+++ app/src/main/java/br/com/caelum/cadastro/activity/ListaAlunosActivity.java
@@ -0,0 +1,16 @@
+package br.com.caelum.cadastro.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import br.com.caelum.cadastro.R;
+
+public class ListaAlunosActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alunos);
+    }
+
+}
