8cbe3272fa73ed054710f6876be1d5016e424fee
wiliamtrancoso <wileleh@gmail.com>
2016-03-02 18:29:37 -0300

    [aula] [wiliam.trancoso]   \_> 2.1 Criando Projeto     \_> layout       \_> add ListView alunos     \_> Activity       \_> criando lista de alunos martelada       \_> recuperando ListView do layout       \_> criado ArrayAdapter       \_> setArrayAdapter da ListView

--- app/src/main/java/br/com/caelum/cadastro/activity/ListaAlunosActivity.java
+++ app/src/main/java/br/com/caelum/cadastro/activity/ListaAlunosActivity.java
@@ -2,15 +2,25 @@ package br.com.caelum.cadastro.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import br.com.caelum.cadastro.R;
 
 public class ListaAlunosActivity extends ActionBarActivity {
 
+    ListView listaAlunos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
+
+        String[] alunos = {"Anderson", "Filipe", "Guilherme"};
+
+        this.listaAlunos = (ListView)findViewById(R.id.lista_alunos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
+        listaAlunos.setAdapter(adapter);
     }
 
 }
