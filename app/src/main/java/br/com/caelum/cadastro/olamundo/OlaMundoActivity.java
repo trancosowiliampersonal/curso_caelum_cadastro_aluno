dc717e2b1e3f6359790ea65eb4ff9b41d445d592
wiliamtrancoso <wileleh@gmail.com>
2016-03-02 15:25:35 -0300

    [master] [wiliam.trancoso]   \_> 1.4 Executando o OlaMundo

---
+++ app/src/main/java/br/com/caelum/cadastro/olamundo/OlaMundoActivity.java
@@ -0,0 +1,18 @@
+package br.com.caelum.cadastro.olamundo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import br.com.caelum.cadastro.R;
+
+/**
+ * Created by Wiliam on 02/03/2016.
+ */
+public class OlaMundoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ola_mundo);
+    }
+}
