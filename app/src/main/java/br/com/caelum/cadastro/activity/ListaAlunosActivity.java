0eff6a89f2e1f30019a58e636c7b4d9f8f2dc4cf
wiliamtrancoso <wileleh@gmail.com>
2016-03-02 18:39:00 -0300

    [aula] [wiliam.trancoso]   \_> 2.3 Integrando o Toast [Incompleto]     \_> implementando o click simples no item da lista     \_> exibindo um toast na implementacao

--- app/src/main/java/br/com/caelum/cadastro/activity/ListaAlunosActivity.java
+++ app/src/main/java/br/com/caelum/cadastro/activity/ListaAlunosActivity.java
@@ -2,8 +2,11 @@ package br.com.caelum.cadastro.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import br.com.caelum.cadastro.R;
 
@@ -16,11 +19,20 @@ public class ListaAlunosActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
 
-        String[] alunos = {"Anderson", "Filipe", "Guilherme"};
+        final String[] alunos = {"Anderson", "Filipe", "Guilherme"};
 
         this.listaAlunos = (ListView)findViewById(R.id.lista_alunos);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
         listaAlunos.setAdapter(adapter);
+
+        listaAlunos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String mensagem = "Posicao Selecionada: " + position;
+                Toast.makeText(ListaAlunosActivity.this, mensagem, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
 }
