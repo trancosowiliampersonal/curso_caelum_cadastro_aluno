69523736ec72b3268c3fd2616120a2c47d7784e7
wiliamtrancoso <wileleh@gmail.com>
2016-03-03 14:54:37 -0300

    [aula] [wiliam.trancoso]   \_> 4.7 Utilizando o DAO para salvar um novo aluno [Incompleto]     \_> add abstractDAO para abstrair o comum entre todos os DAOs

---
+++ app/src/main/java/br/com/caelum/cadastro/dao/AbstractDAO.java
@@ -0,0 +1,13 @@
+package br.com.caelum.cadastro.dao;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public abstract class AbstractDAO extends SQLiteOpenHelper {
+    private static final String NAME = "CadastroCaelum";
+    private static final int VERSION = 1;
+
+    public AbstractDAO(Context context) {
+        super(context, NAME, null, VERSION);
+    }
+}
