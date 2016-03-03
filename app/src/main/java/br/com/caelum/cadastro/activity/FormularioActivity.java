1be0cfe379b5d503c6ca9f09a76135844a6c10c2
wiliamtrancoso <wileleh@gmail.com>
2016-03-03 11:53:18 -0300

    [aula] [wiliam.trancoso]   \_> 3.1 Criando uma tela para adicionar alunos [Incompleto]     \_> criacao do xml     \_> criacao da Activity     \_> declaracao da activity no manifest

---
+++ app/src/main/java/br/com/caelum/cadastro/activity/FormularioActivity.java
@@ -0,0 +1,18 @@
+package br.com.caelum.cadastro.activity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.ActionBarActivity;
+
+import br.com.caelum.cadastro.R;
+
+/**
+ * Created by Wiliam on 03/03/2016.
+ */
+public class FormularioActivity extends ActionBarActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+    }
+}
