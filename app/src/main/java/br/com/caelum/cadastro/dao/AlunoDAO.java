ad74de8ea48dd67303eeacf5a53e3af70c377839
wiliamtrancoso <wileleh@gmail.com>
2016-03-03 14:59:52 -0300

    [aula] [wiliam.trancoso]   \_> 4.7 Utilizando o DAO para salvar um novo aluno [Incompleto]     \_> implementado onCreate e onUpgrade

--- app/src/main/java/br/com/caelum/cadastro/dao/AlunoDAO.java
+++ app/src/main/java/br/com/caelum/cadastro/dao/AlunoDAO.java
@@ -2,11 +2,10 @@ package br.com.caelum.cadastro.dao;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-
-import br.com.caelum.cadastro.modelo.Aluno;
 
 public class AlunoDAO extends AbstractDAO{
+
+    private static final String TABLE = "ALUNO";
 
     public AlunoDAO(Context context){
         super(context);
@@ -14,11 +13,20 @@ public class AlunoDAO extends AbstractDAO{
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        String s =  " CREATE TABLE " + TABLE + "(" +
+                " id INTEGER PRIMARY KEY, " +
+                " nome TEXT NOT NULL, " +
+                " telefone TEXT, " +
+                " endereco TEXT, " +
+                " site TEXT, " +
+                " nota REAL)";
 
+        db.execSQL(s);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE);
+        onCreate(db);
     }
 }
