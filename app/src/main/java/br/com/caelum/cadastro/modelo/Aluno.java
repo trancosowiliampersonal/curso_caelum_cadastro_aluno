f97e466fe6d4fefc21245020387bdbac014b442c
wiliamtrancoso <wileleh@gmail.com>
2016-03-03 14:42:53 -0300

    [aula] [wiliam.trancoso]   \_> 4.2 Nossa Classe de modelo e extraindo os dados do formulario [Incompleto]     \_> add logica do model Aluno

--- app/src/main/java/br/com/caelum/cadastro/modelo/Aluno.java
+++ app/src/main/java/br/com/caelum/cadastro/modelo/Aluno.java
@@ -1,7 +1,58 @@
 package br.com.caelum.cadastro.modelo;
 
-/**
- * Created by Wiliam on 03/03/2016.
- */
 public class Aluno {
+    private Long id;
+    private String nome;
+    private String telefone;
+    private String endereco;
+    private String site;
+    private float nota;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    public float getNota() {
+        return nota;
+    }
+
+    public void setNota(float nota) {
+        this.nota = nota;
+    }
 }
