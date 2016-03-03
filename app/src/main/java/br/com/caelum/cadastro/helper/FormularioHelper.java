b0c05d43874da4207da7c5d8ed64affb960d3b3b
wiliamtrancoso <wileleh@gmail.com>
2016-03-03 14:10:02 -0300

    [aula] [wiliam.trancoso]   \_> 4.2 Nossa Classe de modelo e extraindo os dados do formulario [Incompleto]     \_> add classe model aluno     \_> add classe helper para o formulario

---
+++ app/src/main/java/br/com/caelum/cadastro/helper/FormularioHelper.java
@@ -0,0 +1,7 @@
+package br.com.caelum.cadastro.helper;
+
+/**
+ * Created by Wiliam on 03/03/2016.
+ */
+public class FormularioHelper {
+}
---
+++ app/src/main/java/br/com/caelum/cadastro/modelo/Aluno.java
@@ -0,0 +1,7 @@
+package br.com.caelum.cadastro.modelo;
+
+/**
+ * Created by Wiliam on 03/03/2016.
+ */
+public class Aluno {
+}
