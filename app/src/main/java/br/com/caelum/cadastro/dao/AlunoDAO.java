8b351923a5273104f115b93df3fa817fbcc0dc0f
wiliamtrancoso <wileleh@gmail.com>
2016-03-03 15:12:40 -0300

    [aula] [wiliam.trancoso]   \_> 4.7 Utilizando o DAO para salvar um novo aluno [Incompleto]     \_> dao inserindo aluno passado por parametro

--- app/src/main/java/br/com/caelum/cadastro/dao/AlunoDAO.java
+++ app/src/main/java/br/com/caelum/cadastro/dao/AlunoDAO.java
@@ -1,7 +1,10 @@
 package br.com.caelum.cadastro.dao;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+
+import br.com.caelum.cadastro.modelo.Aluno;
 
 public class AlunoDAO extends AbstractDAO{
 
@@ -28,5 +31,22 @@ public class AlunoDAO extends AbstractDAO{
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE);
         onCreate(db);
+    }
+
+    public void inserir(Aluno aluno){
+        ContentValues cv = alunoToContentValues(aluno);
+        aluno.setId(getWritableDatabase().insert(TABLE, null, cv));
+    }
+
+    private ContentValues alunoToContentValues(Aluno aluno){
+        ContentValues cv = new ContentValues();
+
+        cv.put("nome", aluno.getNome());
+        cv.put("telefone", aluno.getTelefone());
+        cv.put("endereco", aluno.getEndereco());
+        cv.put("site", aluno.getSite());
+        cv.put("nota", aluno.getNota());
+
+        return cv;
     }
 }
