5134309a2aef6b916c1c2f3f6be17d9481ad38dc
wiliamtrancoso <wileleh@gmail.com>
2016-03-06 19:33:58 -0300

    [aula] [wiliam.trancoso]   \_> 6.2 Editando Aluno Selecionado [Incompleto]     \_> Aluno implementando serializeble para poder ser enviado de uma activity para outra

--- app/src/main/java/br/com/caelum/cadastro/modelo/Aluno.java
+++ app/src/main/java/br/com/caelum/cadastro/modelo/Aluno.java
@@ -1,6 +1,8 @@
 package br.com.caelum.cadastro.modelo;
 
-public class Aluno {
+import java.io.Serializable;
+
+public class Aluno implements Serializable{
     private Long id;
     private String nome;
     private String telefone;
